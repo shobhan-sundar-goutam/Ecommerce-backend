93a82eef3a6aea3fa72874b65a202accaeaac21d
Shobhan Sundar Goutam <shobhangoutam@gmail.com>
2023-04-30 21:19:54 +0530

    password hashing

--- models/user.schema.js
+++ models/user.schema.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import validator from 'validator';
+import bcrypt from 'bcryptjs';
 import AuthRoles from '../utils/authRoles';
 
 const userSchema = new mongoose.Schema(
@@ -41,5 +42,12 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 10);
+  return next();
+});
 
 export default mongoose.model('User', userSchema);
