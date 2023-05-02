13a7997218fab14c0017f442e29b7dbb09f10fb4
Shobhan Sundar Goutam <shobhangoutam@gmail.com>
2023-05-02 23:04:15 +0530

    Logout

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -56,3 +56,16 @@ export const login = asyncHandler(async (req, res) => {
 
   sendToken(user, 200, res);
 });
+
+// Logout
+export const logout = asyncHandler(async (req, res) => {
+  res.cookie('token', null, {
+    expires: new Date(Date.now()),
+    httpOnly: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    message: 'Logged out',
+  });
+});
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { signUp, login } from '../controllers/user.controller.js';
+import { signUp, login, logout } from '../controllers/user.controller.js';
 
 const router = express.Router();
 
 router.post('/auth/signup', signUp);
 router.post('/auth/login', login);
+router.get('/auth/logout', logout);
 
 export default router;
