c59187f8570da276436e39b348a9d5b4292551cb
Shobhan Sundar Goutam <shobhangoutam@gmail.com>
2023-05-03 23:30:39 +0530

    send mailer done

--- config/index.js
+++ config/index.js
@@ -10,6 +10,13 @@ const config = {
   JWT_EXPIRY: process.env.JWT_EXPIRY,
 
   COOKIE_EXPIRY: process.env.COOKIE_EXPIRY,
+
+  SMTP_MAIL_SERVICE: process.env.SMTP_MAIL_SERVICE,
+  SMTP_MAIL_HOST: process.env.SMTP_MAIL_HOST,
+  SMTP_MAIL_PORT: process.env.SMTP_MAIL_PORT,
+  SMTP_MAIL_USERNAME: process.env.SMTP_MAIL_USERNAME,
+  SMTP_MAIL_PASSWORD: process.env.SMTP_MAIL_PASSWORD,
+  SMTP_MAIL_EMAIL: process.env.SMTP_MAIL_EMAIL,
 };
 
 export default config;
